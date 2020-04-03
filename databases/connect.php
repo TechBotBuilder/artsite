328fdc3dff6799baded8c306736661d63e193b17
TechBotBuilder <techbotbuilder@gmail.com>
2020-04-03 17:46:57 -0400

    debug sql

--- databases/connect.php
+++ databases/connect.php
@@ -2,9 +2,11 @@
 
 namespace database;
 
+echo "enter connect";
 $opts = parse_ini_file('db.ini');
-
+var_dump($opts);
 function connect(){
+	echo "connect called";
 	static $conn = null;
 	if($conn==null){
 		$conn = new \PDO('mysql:host='.$opts['host'].';dbname='.$opts['database'],
--- databases/query.php
+++ databases/query.php
@@ -3,12 +3,18 @@
 //run a SQL query against the database.
 
 namespace database;
+echo "enter query.php";
+
 require_once "connect.php";
 
 
 function query($sql, $data = array()){
+	echo "running query on ".$sql." with data: "; var_dump($data);
     $conn = connect();
+    echo "about to prepare sql";
     $conn->prepare($sql);
+    echo "about to execute sql";
     $conn->execute($data);
+    echo "returning sql query";
     return $conn->fetchAll(\PDO::FETCH_ASSOC);
 }
