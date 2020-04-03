399ba0620903ac395af359894fc63987c89051ac
TechBotBuilder <techbotbuilder@gmail.com>
2020-04-03 19:34:56 -0400

    remove debug prints from list_works.php

--- databases/list_works.php
+++ databases/list_works.php
@@ -6,10 +6,10 @@
 //database/query  look up MySQL select a range of primary keys
 //
 namespace database;
-echo "list_works.php";
+
 require_once "query.php";
-echo "get results";
+
 $get_resultsSQL = "SELECT * FROM images LIMIT 0, 20;";
-echo "calling query";
 $results = query($get_resultsSQL);
+
 var_dump($results);
