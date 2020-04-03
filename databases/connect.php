17ea0be6fc164e99183a9c087f2d95269b92e455
TechBotBuilder <techbotbuilder@gmail.com>
2020-04-03 17:52:41 -0400

    simplify connect

--- databases/connect.php
+++ databases/connect.php
@@ -7,10 +7,10 @@ $opts = parse_ini_file('db.ini');
 var_dump($opts);
 function connect(){
 	echo "connect called";
-	static $conn = null;
-	if($conn==null){
+	//static $conn = null;
+	//if($conn==null){
 		$conn = new \PDO('mysql:host='.$opts['host'].';dbname='.$opts['database'],
 		    $opts['user'], $opts['password']);
-	}
+	//}
 	return $conn;
 }
