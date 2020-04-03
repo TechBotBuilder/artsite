218d660b8be421c98d63768a4399fa0d524df0c2
tddubr <63024081+tddubr@users.noreply.github.com>
2020-04-03 19:11:24 -0400

    replaced prepared statement with direct query
    its a hunch

--- databases/query.php
+++ databases/query.php
@@ -12,9 +12,10 @@ function query($sql, $data = array()){
 	echo "running query on ".$sql." with data: "; var_dump($data);
     $conn = connect();
     echo "about to prepare sql";
-	$statement=$conn->prepare($sql);
+	/*$statement=$conn->prepare($sql);
     echo "about to execute sql";
-    $statement->execute($data);
+    $statement->execute($data); */
+	$statement = $conn->query($sql);
     echo "returning sql query";
     return $statement->fetchAll(\PDO::FETCH_ASSOC);
 }
