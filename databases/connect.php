65fa159644bb7148dca0fee2365603096f74fbaa
TechBotBuilder <techbotbuilder@gmail.com>
2020-04-03 18:22:54 -0400

    turn on php log

--- databases/connect.php
+++ databases/connect.php
@@ -4,8 +4,12 @@ namespace database;
 
 echo "enter connect";
 $opts = parse_ini_file('db.ini');
-var_dump($opts);
+echo "options connect to database ".$opts["host"];
 function connect(){
+	ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
+error_reporting(E_ALL);
 	echo "connect called";
 	//static $conn = null;
 	//if($conn==null){
@@ -13,7 +17,6 @@ function connect(){
 		$conn = new \PDO('mysql:host=' . $opts['host'],
 		    $opts['user'], $opts['password']);
 		$conn->prepare('use ' . $opts['database'] . ';' );
-		$conn->execute();
     }catch($e){
     	echo 'error' . $e->getMessage();
     }
