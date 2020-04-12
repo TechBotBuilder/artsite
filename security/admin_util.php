4b89322a7d4aca8ad933087e4a9d77691f938af5
TechBotBuilder <techbotbuilder@gmail.com>
2020-04-12 00:21:46 -0400

    fix more syntax typos

--- security/admin_util.php
+++ security/admin_util.php
@@ -10,33 +10,33 @@ class Admin {
 	
 	public function __construct(){
 		require_once '../databases/query.php';
-		$res = \database\query("SELECT val FROM config WHERE nam='admin'")[0];
+		$res = \database\query("SELECT val FROM config WHERE nam='admin'")[0]['val'];
 		$dat = json_decode($res);
-		$this->$username = $dat['username'];
-		$this->$password_h = $dat['password_h'];
-		$this->$email = $dat['email'];
+		$this->username = $dat['username'];
+		$this->password_h = $dat['password_h'];
+		$this->email = $dat['email'];
 	}
 	
 	public function checkPassword($candidate){
-		return password_verify($candidate, $this->$password_h);
+		return password_verify($candidate, $this->password_h);
 	}
-	public function getUsername(){return $this->$username;}
-	public function getEmail(){return $this->$email;}
+	public function getUsername(){return $this->username;}
+	public function getEmail(){return $this->email;}
 	
 	public function setPassword($newpass)
 	{
 		$dat = array();
-		$dat['username'] = $this->$username;
+		$dat['username'] = $this->username;
 		$dat['password_h'] = password_hash($newpass);
-		$dat['email'] = $this->$email;
+		$dat['email'] = $this->email;
 		$put = json_encode($dat);
 		\database\query("UPDATE config SET val = :put WHERE nam='admin'", ['put'=>$put]);
 	}
 	public function setEmail($email)
 	{
 		$dat = array();
-		$dat['username'] = $this->$username;
-		$dat['password_h'] = $this->$password_h;
+		$dat['username'] = $this->username;
+		$dat['password_h'] = $this->password_h;
 		$dat['email'] = $email;
 		$put = json_encode($dat);
 		\database\query("UPDATE config SET val = :put WHERE nam='admin'", ['put'=>$put]);
